f057670b57426b6c55bc38ae98ecc3773761dc9a
Scott Dixon <scott@howrefreshing.com.au>
2018-02-13 04:49:49 +1000

    Updated for Ruby TDD lesson

--- calculator_test.rb
+++ calculator_test.rb
@@ -1,15 +1,15 @@
 require 'test/unit'
-require_relative 'fancyapp'
+require_relative 'calculator'
 
-class FancyappTest < Test::Unit::TestCase
+class CalculatorTest < Test::Unit::TestCase
   def test_add
     total = add(1,2)
     assert_equal(3, total)
   end
 
-  def test_minus
-    total = minus(10, 5)
+  def test_subtract
+    total = subtract(10, 5)
     assert_equal(5, total)
   end
-  
+
 end
