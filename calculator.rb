4179a22aa68b7b626e938f73cd03bac5165ca601
thanhthedev <huythanhdao@gmail.com>
2018-09-03 16:37:47 +1000

    v1

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,19 @@ def add (a, b)
   a + b
 end
 
-# Other methods here :)
+def subtract (a, b)
+  a - b
+end
+
+def multiply (a, b)
+  a * b
+end
+
+def divide (a, b)
+  a / b
+end
+
+def square (a, b)
+  a ** b
+end
+  # Other methods here :)
